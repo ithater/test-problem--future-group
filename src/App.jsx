7d92b46d7f424b676d811665a557631c5740bc85
ithater <ithater.iwnl@gmail.com>
2020-12-26 10:56:15 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+// styled-components - достаточно удобное средство стилизации
+// поскольку удобно пользоваться props и делать код более выраженным.
+
+// react-router-dom - средство роутинга
+// Удобное логирование.
+
+import GlobalStyle from './GlobalStyle';
+
+import Error from './components/Error/Error';
+import Choice from './components/Choice/Choice';
+import Home from './components/Home/Home';
+
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Redirect,
+} from 'react-router-dom';
 
 const App = () => {
-	return <div>Content</div>;
+	const [userData, setUserData] = useState(null);
+	const [error, setError] = useState(null);
+
+	return (
+		<>
+			<GlobalStyle />{' '}
+			<Router>
+				<Switch>
+					{error && <Redirect exact from="/" to="/error" />}
+
+					<Route exact path="/">
+						<Choice setUserData={setUserData} setError={setError} />
+					</Route>
+
+					<Route exact path="/error">
+						{error ? <Error error={error} /> : <Redirect to="/" />}
+					</Route>
+
+					<Route exact path="/home">
+						{userData ? <Home userData={userData} /> : <Redirect to="/" />}
+					</Route>
+				</Switch>
+			</Router>
+		</>
+	);
 };
 
 export default App;
