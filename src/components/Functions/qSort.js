ddeee0bc40343050d52e6afb49c9f2b4e8b6e505
ithater <ithater.iwnl@gmail.com>
2020-12-26 10:56:09 +0300

    quick sort func

---
+++ src/components/Functions/qSort.js
@@ -0,0 +1,27 @@
+const qSort = (list, key, type = 'increase') => {
+	// list - array
+	// key - ключ элемента стписка
+	// type - 'decrease' - убывание | 'increase' - возрастание;
+	// Быстрая сортировка основываясь на значении из ключа.
+	// При вложенности ключей {somekey: {val: { id: 100, name: 'Igor'}}}
+	// нужно переписать код с использованием lodash.get или подобным алгоритмом.
+	list = [...list]
+	const sort = (list, key) => {
+		if (list.length < 2) return list;
+		const middleIndex = Math.floor(list.length / 2);
+		const pivot = list.splice(middleIndex, 1)[0];
+		const pivotValue = pivot[key];
+		const smaller = list.filter(item => item[key] <= pivotValue);
+		const bigger = list.filter(item => item[key] > pivotValue);
+		return [...sort(smaller, key), pivot, ...sort(bigger, key)];
+	};
+
+	const sortedList = sort(list, key);
+	console.log('sort func');
+
+	if (type === 'decrease') sortedList.reverse();
+
+	return sortedList;
+};
+
+export default qSort;
